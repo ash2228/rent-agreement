13d6fdac3c1d2b6de0883fb87c34582548e08661
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-11-09 19:07:45 +0530

    compare agreement done for pc screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         'fade-gradient': 'linear-gradient(to bottom, #232D32, rgba(85, 95, 100, 0.4), rgba(85, 95, 100, 0))', // Fades to transparent
         'fade-blue': 'linear-gradient(to bottom, blue, rgba(0, 0, 255, 0.4), rgba(0, 0, 255, 0))', // Fades to transparent
       },
+    
     },
   },
   plugins: [],
